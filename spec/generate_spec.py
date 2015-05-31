9d2c92f83498adfa5714c27ebfd4553775d871b5
Manuel Cerón <ceronman@gmail.com>
2015-05-31 21:02:20 +0200

    Script to generate the spec is almost complete

--- spec/generate_spec.py
+++ spec/generate_spec.py
@@ -1,13 +1,35 @@
-import textwrap
 import xml.etree.ElementTree as ElementTree
 
 
-class Method:
-    def __init__(self, domain, name, args, result):
-        self.domain = domain
-        self.name = name
-        self.args = args,
-        self.result = result
+def parse_field_type(field):
+    if (field.tag == 'ref'):
+        return field.text.strip()
+    if field.find('ref') is not None:
+        return parse_field_type(field.find('ref'))
+    elif field.find('list') is not None:
+        item = parse_field_type(field.find('list'))
+        return 'List[{}]'.format(item)
+    elif field.find('map') is not None:
+        key = parse_field_type(field.find('./map/key'))
+        value = parse_field_type(field.find('./map/value'))
+        return 'Dict[{}:{}]'.format(key, value)
+    elif field.find('union') is not None:
+        values = [parse_field_type(v) for v in field.find('union')]
+        return 'Union[{}]:'.format(','.join(values))
+    else:
+        raise Exception("Unknow field type")
+
+
+def parse_element_doc(element):
+    lines = []
+    paragraphs = element.findall('p')
+    for p in paragraphs:
+        parts = []
+        for part in p.itertext():
+            clean_part = ' '.join(part.split()).strip()
+            parts.append(clean_part)
+        lines.append(' '.join(parts))
+    return lines
 
 if __name__ == '__main__':
     root = ElementTree.parse('spec_input.html').getroot()
@@ -16,18 +38,23 @@ if __name__ == '__main__':
         for request in domain.findall('request'):
             print(domain.get('name'), request.get('method'))
 
-            if request.findall('result'):
-                print('\t has result')
+            result = None
+            result_doc = None
+            if request.find('./result/field'):
+                result = parse_field_type(request.find('./result/field'))
+                result_doc = parse_element_doc(request.find('result'))
+            print('\tresult:', repr(result))
+            print('\tresult doc:', repr(result_doc))
 
-            params = request.findall('params')
-            if params:
-                print('\t has params')
+            params = {}
+            param_docs = {}
+            for field in request.findall('./params/field'):
+                param_name = field.get('name')
+                params[param_name] = parse_field_type(field)
+                param_docs[param_name] = parse_element_doc(field)
 
-            paragraphs = request.findall('p')
-            for p in paragraphs:
-                parts = []
-                for part in p.itertext():
-                    clean_part = ' '.join(part.split()).strip()
-                    parts.append(clean_part)
-                parts = ' '.join(parts)
-                print(textwrap.fill(parts, initial_indent='    '))
+            print('\tparams:', repr(params))
+            print('\tparam docs:', repr(param_docs))
+
+            doc_lines = parse_element_doc(request)
+            print('\tdoc:', repr(doc_lines))
